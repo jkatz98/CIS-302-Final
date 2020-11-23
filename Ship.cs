c3778ea217ff33f9b5b2f13092a29271ac92dbc1
William Davis <59844103+billy072@users.noreply.github.com>
2020-11-22 22:44:40 -0500

    Length integrity
    Added a new class variable currentLength, ensures the program can know the ships length originally and currently.

--- Ship.cs
+++ Ship.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,34 +10,40 @@ namespace Battleship_Game
     {
         private string name;
         private int length;
+        private int currentLength;
         private Boolean sunk;
 
         public Ship(string s, int i)
         {
             this.name = s;
             this.length = i;
+            this.currentLength = length;
             this.sunk = false;
         }
 
-        private string getName()
+        public string getName()
         {
             return name;
         }
 
-        private int getLength()
+        public int getLength()
         {
             return length;
         }
 
-        private Boolean getSunk()
+        public int getCurrentLength() {
+            return currentLength;
+        }
+
+        public Boolean getSunk()
         {
             return sunk;
         }
 
         private void hit()
         {
-            length = length - 1;
-            if (length == 0)
+            currentLength = currentLength - 1;
+            if (currentLength == 0)
             {
                 sunk = true;
             }
